c2c00c9d4d62efd0fa0a88fa928872cc4de7b3d1
B-Musick <bmuze1@gmail.com>
2020-03-14 16:05:10 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var expect      = require('chai').expect;
+var cors        = require('cors');
+
+var apiRoutes         = require('./routes/api.js');
+var fccTestingRoutes  = require('./routes/fcctesting.js');
+var runner            = require('./test-runner');
+
+var app = express();
+
+// Packages added
+var mongoose = require('mongoose');
+let helmet = require('helmet');
+let dotenv = require('dotenv');
+
+// Configurations added
+dotenv.config();
+app.use(helmet.frameguard({ action: "sameorigin" }))
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/fcc_message_board',{ useNewUrlParser: true, useUnifiedTopology: true})
+
+// CONNECT TO MONGODB ATLAS DATABASE - pass URI key to connect
+// mongoose.connect(process.env.DATABASE, {
+//   userNewUrlParser: true,
+//   useCreateIndex: true
+// }).then(async () => {
+//   console.log("Connected to DB!");
+//   await Stock.collection.drop();
+// }).catch(err => {
+//   console.log("Error: ", err.message);
+// });
+
+//Sample front-end
+app.route('/b/:board/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/board.html');
+  });
+app.route('/b/:board/:threadid')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/thread.html');
+  });
+
+//Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);
+
+//Sample Front-end
+
+    
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+//Start our server and tests!
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app; //for testing
